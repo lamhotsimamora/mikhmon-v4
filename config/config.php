ad6d43a66d96ecc85f03d4ad78ba3931687d1630
lamhotsimamora <lamhotsimamora36@gmail.com>
2024-12-14 16:34:33 +0700

    +

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['Panti'] = array ('1'=>'Panti!heq095frrf4.sn.mynetname.net','Panti@|@pdr','Panti#|#qJWkaWNlbGY=','Panti%Panti','Panti^login.net','Panti&Rp','Panti*081399453242','Panti(','Panti)','Panti=30','Panti@!@disable','Panti#!#');
